0fca8505dba537b8bdba35255512ecd77ebd6c6c
John Otander <johnotander@gmail.com>
2014-06-05 10:31:56 -0600

    Add module utility for email validation.

--- spec/email_format_validator_spec.rb
+++ spec/email_format_validator_spec.rb
@@ -18,7 +18,7 @@ describe EmailFormatValidator do
 
   context "with invalid emails" do
 
-    let(:invalid_emails) { %w(invalid_email@ 
+    let(:invalid_emails) { %w(invalid_email@
                               another_invalid_email@@email.email
                               invalid
                               bad@email@here
